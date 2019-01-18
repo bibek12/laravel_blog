dd385cc9e20396d39cd51167e701047d568dd52c
bibek12 <sbibek100@gmail.com>
2019-01-18 23:38:05 +0545

    filtering the post by categories part 3 (completed)

--- app/Http/Controllers/BlogController.php
+++ app/Http/Controllers/BlogController.php
@@ -14,34 +14,39 @@ class BlogController extends Controller
     protected $limit=3;
     public function index(){
 
-        $categories=Category::with(['posts'=>function($query){
-                $query->published();
-        }])->orderBY('title','asc')->get();
+        
      
         $posts=POST::with('user')
        ->latestFirst()
        ->published()
        ->simplePaginate($this->limit);
-       return view("blog.index",compact('posts','categories'));
+       return view("blog.index",compact('posts'));
        
     }
 
-    public function category($id){
-
-       $categories=Category::with(['posts'=>function($query){
-                $query->published();
-            }])->orderBY('title','asc')->get();
+    public function category(Category $category){
+        $categoryName=$category->title;
+      
      
-        $posts=POST::with('user')
-       ->latestFirst()
-       ->published()
-       ->where('category_id',$id)
-       ->simplePaginate($this->limit);
-       return view("blog.index",compact('posts','categories'));
-       
+    //     $posts=POST::with('user')
+    //    ->latestFirst()
+    //    ->published()
+    //    ->where('category_id',$id)
+    //    ->simplePaginate($this->limit);
+       // \DB::enableQueryLog();
+        $posts=$category->posts()
+                        ->with('user')
+                        ->latestFirst()
+                        ->published()
+                        ->simplePaginate($this->limit);
+      
+       return view("blog.index",compact('posts','categoryName'));
+       //dd(\DB::getQueryLog());
     }
 
     public function show(Post $post){
+       
+     
        return view("blog.show",compact('post'));
     }
 }
